024cecb55e46fc3c36d2dd9aaa7036a4b1fcc9ba
Alphyn Rex <136186684+Alphyn-reX@users.noreply.github.com>
2023-06-15 12:35:51 +0530

    Update Disclaimer.sh

--- Disclaimer.sh
+++ Disclaimer.sh
@@ -1,2 +1,3 @@
 echo "Reach_you is paylaod create tool...."
-echo "This tool is designed for security testing in an authorized-simulated cyberattack Attacking targets without prior mutual consent is illegal!"
+echo "This tool is designed for security testing in an authorized-
+simulated cyberattack Attacking targets without prior mutual consent is illegal!"
